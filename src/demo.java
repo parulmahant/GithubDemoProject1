3d0c292945b7438f127089bb86af989bdcb48447
parulmahant <parulmahant665@gmail.com>
2022-01-19 16:37:23 -0500

    First Commit

---
+++ src/demo.java
@@ -0,0 +1,17 @@
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String you="yes";
+if(you=="yes") {
+	
+	System.out.println("correct");
+}
+else {
+	System.out.println("correct");
+	
+}
+	}
+
+}
